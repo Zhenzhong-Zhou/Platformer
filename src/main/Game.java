2f6740510491f79698f2b94b0fa06ef54492f436
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-24 22:36:48 -0600

    Refactor game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -23,12 +23,22 @@ public class Game implements Runnable{
         double timePerFrame = 1000000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
         long now = System.nanoTime();
+
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
         // noinspection InfiniteLoopStatement
         while(true) {
             now = System.nanoTime();
             if(now - lastFrame >= timePerFrame) {
                 scene.repaint();
                 lastFrame = now;
+                frames++;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
             }
         }
     }
