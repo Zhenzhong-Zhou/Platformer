7ffc6c0dc21956cef47b6f312aac2b1fa71f69c9
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-25 14:14:29 -0600

    Refactor codes

--- src/utilities/HelpMethods.java
+++ src/utilities/HelpMethods.java
@@ -4,10 +4,10 @@ import static main.Game.*;
 
 public class HelpMethods {
     public static boolean CanMoveHere(float x, float y, int width, int height, int[][] levelData) {
-        if(!IsSolid(x,y , levelData)) {
-            if(!IsSolid(x+width, y+height,levelData)) {
-                if(!IsSolid(x+width, y,levelData)) {
-                    return ! IsSolid(x, y + height, levelData);
+        if(IsSolid(x, y, levelData)) {
+            if(IsSolid(x + width, y + height, levelData)) {
+                if(IsSolid(x + width, y, levelData)) {
+                    return IsSolid(x, y + height, levelData);
                 }
             }
         }
@@ -16,10 +16,10 @@ public class HelpMethods {
 
     private static boolean IsSolid(float x, float y, int[][] levelData) {
         if(x <0 || x>=GAME_WIDTH){
-            return true;
+            return false;
         }
         if(y<0|| y>= GAME_HEIGHT) {
-            return true;
+            return false;
         }
 
         float xIndex = x / TILES_SIZE;
@@ -27,9 +27,6 @@ public class HelpMethods {
 
         int value = levelData[(int) yIndex][(int) xIndex];
 
-        if(value >= 48 || value < 0 || value != 11) {
-            return true;
-        }
-        return false;
+        return value == 11;
     }
 }
