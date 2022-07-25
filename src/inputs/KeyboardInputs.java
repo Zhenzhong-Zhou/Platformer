700c1ed8cbeee506cdf278a9621c1927d40e766f
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-25 00:33:56 -0600

    Remove unused codes

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -24,19 +24,15 @@ public class KeyboardInputs implements KeyListener {
         switch(e.getKeyCode()) {
             case KeyEvent.VK_W -> {
                 scene.setDirection(UP);
-                System.out.println("W is pressed!");
             }
             case KeyEvent.VK_A -> {
                 scene.setDirection(LEFT);
-                System.out.println("A is pressed!");
             }
             case KeyEvent.VK_S -> {
                 scene.setDirection(DOWN);
-                System.out.println("S is pressed!");
             }
             case KeyEvent.VK_D -> {
                 scene.setDirection(RIGHT);
-                System.out.println("D is pressed!");
             }
             default -> {
             }
