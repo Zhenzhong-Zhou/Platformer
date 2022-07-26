bd96cc2e7da01e3bed28c6ad0e704723c7abc339
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-26 11:17:10 -0600

    Refactor res folder

--- src/utilities/LoadSave.java
+++ src/utilities/LoadSave.java
@@ -8,19 +8,19 @@ import java.io.InputStream;
 
 public class LoadSave {
     public static final String PLAYER_ATLAS = "player_sprites.png";
-    public static final String LEVEL_ATLAS = "outside_sprites.png";
+    public static final String LEVEL_ATLAS = "environment/outside_sprites.png";
     //    public static final String LEVEL_DEFAULT_DATA = "level_one_data.png";
     public static final String LEVEL_DEFAULT_DATA = "level_one_data_long.png";
-    public static final String MENU_BUTTONS = "button_atlas.png";
-    public static final String MENU_BACKGROUND = "menu_background.png";
-    public static final String MENU_BACKGROUND_IMAGE = "background_menu.png";
-    public static final String PAUSE_BACKGROUND = "pause_background.png";
-    public static final String SOUND_BUTTONS = "sound_button.png";
-    public static final String UTIL_BUTTONS = "urm_buttons.png";
-    public static final String VOLUME_BUTTONS = "volume_buttons.png";
-    public static final String PLAY_BG_IMAGE = "playing_bg_img.png";
-    public static final String BIG_CLOUDS = "big_clouds.png";
-    public static final String SMALL_CLOUDS = "small_clouds.png";
+    public static final String MENU_BUTTONS = "gui/menu/button_atlas.png";
+    public static final String MENU_BACKGROUND = "gui/menu/menu_background.png";
+    public static final String MENU_BACKGROUND_IMAGE = "gui/menu/background_menu.png";
+    public static final String PAUSE_BACKGROUND = "gui/pause/pause_background.png";
+    public static final String SOUND_BUTTONS = "gui/pause/sound_button.png";
+    public static final String UTIL_BUTTONS = "gui/pause/urm_buttons.png";
+    public static final String VOLUME_BUTTONS = "gui/pause/volume_buttons.png";
+    public static final String PLAY_BG_IMAGE = "environment/playing_bg_img.png";
+    public static final String BIG_CLOUDS = "environment/big_clouds.png";
+    public static final String SMALL_CLOUDS = "environment/small_clouds.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage image = null;
